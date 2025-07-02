9b7c6fc7733427a12b66c1f3893b9a38af0a9c78
Doquynh2603 <147628345+Doquynh2603@users.noreply.github.com>
2025-07-02 23:07:23 +0700

    exercise 1.11

--- part 1/courseinfor/src/App.jsx
+++ part 1/courseinfor/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 const StatisticLine = (props) => {
   return (
-    <div>
-      <p>{props.text} {props.value}</p>
-    </div>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 const Statistics = (props) => {
@@ -13,14 +14,17 @@ const Statistics = (props) => {
     )
   }
   return (
-    <div>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="total" value={props.total} />
-      <StatisticLine text="average" value={props.average} />
-      <StatisticLine text="positive" value={props.positive} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="total" value={props.total} />
+        <StatisticLine text="average" value={props.average} />
+        <StatisticLine text="positive" value={props.positive} />
+      </tbody>
+
+    </table>
   )
 }
 const App = () => {
