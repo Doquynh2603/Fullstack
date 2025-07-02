f2929b9838ba481fd2fda93988aec3302181fd0f
Doquynh2603 <147628345+Doquynh2603@users.noreply.github.com>
2025-07-02 22:31:31 +0700

    exercise 1.7

--- part 1/courseinfor/src/App.jsx
+++ part 1/courseinfor/src/App.jsx
@@ -5,7 +5,12 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const positive = total === 0 ? 0 : (good / total) * 100
+  console.log('total', total)
+  console.log('average', average)
+  console.log('positive', positive)
   return (
     <div>
       <h1>give feedback</h1>
@@ -16,6 +21,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
