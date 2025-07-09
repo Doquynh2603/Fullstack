d44451e5171b330a14cf06d76b42664360621a99
Doquynh2603 <147628345+Doquynh2603@users.noreply.github.com>
2025-07-09 09:20:54 +0700

    exercise 2.19

--- part 2/countries/src/App.jsx
+++ part 2/countries/src/App.jsx
@@ -5,6 +5,7 @@ import Country from './component/Country'
 function App() {
   const [filter, setFilter] = useState('')
   const [countries, setCountries] = useState([])
+  const [showCountries, setShowCountries] = useState([]) // lưu tên của các quốc gia được hiển thị chi tiết
   useEffect(() => {
     axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
       .then(res => {
@@ -12,6 +13,15 @@ function App() {
       })
   }, [])
   const subFilter = filter.trim() ? countries.filter(c => c.name.common.toLowerCase().includes(filter.toLowerCase())) : []
+
+  const handleShow = (name) => {
+    if (showCountries.includes(name)) {
+      setShowCountries(showCountries.filter(c => c !== name)) // ẩn thông tin của các quốc gia
+    }
+    else {
+      setShowCountries([...showCountries, name])
+    }
+  }
   let content
   if (filter.trim() && subFilter.length > 10) {
     content = <p>Too many matches, specify another filter</p>
@@ -19,12 +29,23 @@ function App() {
     console.log(Object.values(subFilter[0].languages))
     content = <Country country={subFilter[0]} />
   } else {
-    content = subFilter.map((c, index) => <p key={index}>{c.name.common}</p>)
+    content = subFilter.map((c, index) =>
+      <div key={index}>
+        <p>
+          {c.name.common}{'  '}
+          <button onClick={() => { handleShow(c.name.common) }}>{showCountries.includes(c.name.common) ? 'Hide' : 'Show'}</button>
+        </p>
+        {showCountries.includes(c.name.common) && <Country country={c} />}
+      </div>
+    )
   }
   return (
     <div>
       <div>
-        find countries <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+        find countries <input value={filter} onChange={(e) => {
+          setFilter(e.target.value)
+          setShowCountries([])
+        }} />
       </div>
       <div>
         {
