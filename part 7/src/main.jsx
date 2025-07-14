f3e52ccd9128a1da05113a280def9128e2a111f6
Doquynh2603 <147628345+Doquynh2603@users.noreply.github.com>
2025-07-14 13:47:33 +0700

    exercise 7.8

--- part 7/src/main.jsx
+++ part 7/src/main.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App_country'
+import App from './App_notes.jsx'
 import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
