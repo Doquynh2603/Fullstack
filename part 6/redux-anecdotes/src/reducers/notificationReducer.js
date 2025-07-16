6a5e35fbcff0d415163f6abfcd1b7a939dfc04cf
Doquynh2603 <147628345+Doquynh2603@users.noreply.github.com>
2025-07-16 22:14:05 +0700

    exercise 6.12

---
+++ part 6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,7 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const notificationReducer = createSlice({
+  name: "notification",
+  initialState: "render here notification...",
+});
+export default notificationReducer.reducer;
